fd53737e6a1215a446ecf0ea4f27078f83f5140c
Nataniel Paiva <nataniel.paiva@maisvida.med.br>
2017-09-18 12:38:34 -0300

    Subindo o projeto

---
+++ README.md
@@ -0,0 +1,14 @@
+# Aula 3
+
+* Introdução ao MongoDB
+* Criar uma conexão com o MongoDB
+* Spring Data Repositories
+* @Document
+
+Links úteis
+---
+
+* https://www.mongodb.com/
+* https://www.mongodb.com/products/compass
+* https://docs.mongodb.com/
+* https://docs.spring.io/spring-data/mongodb/docs/1.10.7.RELEASE/reference/html/
---
+++ src/main/java/io/spring/aula/natan/service/UsuarioService.java
@@ -0,0 +1,43 @@
+/*
+*
+* Aula com Nataniel Paiva
+*
+* Esse projeto poderá ser distribuído da forma que você achar melhor
+* O importante é que você aprenda de verdade!
+*
+ */
+package io.spring.aula.natan.service;
+
+import io.spring.aula.natan.entity.Usuario;
+import io.spring.aula.natan.repository.UsuarioRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author Nataniel Paiva <nataniel.paiva@gmail.com>
+ */
+@Service
+public class UsuarioService {
+
+    @Autowired
+    UsuarioRepository usuarioRepository;
+
+    public List<Usuario> listaUsuario() {
+        return usuarioRepository.findAll();
+    }
+
+    public Usuario salvarUsuario(Usuario usuarioAdd) {
+        return usuarioRepository.save(usuarioAdd);
+    }
+
+    public void deleteUsuario(String id) {
+        usuarioRepository.delete(id);
+    }
+
+    public Usuario getById(String id) {
+        return usuarioRepository.findOne(id);
+    }
+
+}
